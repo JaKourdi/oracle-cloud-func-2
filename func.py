4c860f483af4cebf874b5128e6ac6d09b4b64df5
ykourdi <ykourdi@riverbed.com>
2022-12-03 20:55:56 +0200

    add req + blog code

--- func.py
+++ func.py
@@ -1,21 +1,41 @@
 import io
 import json
+import logging
 
+import oci
 from fdk import response
 
 def handler(ctx, data: io.BytesIO=None):
-    print("Entering Python Hello World handler", flush=True)
-    name = "World"
+    if None == ctx.RequestURL():
+        return "Function loaded properly but not invoked via an HTTP request."
+    signer = oci.auth.signers.get_resource_principals_signer()
+    logging.getLogger().info("URI: " + ctx.RequestURL() )
+    config = {
+        # update with your tenancy's OCID
+        "tenancy": "ocid1.tenancy.oc1..XXXXXXX",
+        # replace with the region you are using
+        "region": "ca-toronto-1"
+    }
     try:
-        body = json.loads(data.getvalue())
-        name = body.get("name")
-    except (Exception, ValueError) as ex:
-        print(str(ex), flush=True)
+        object_storage = oci.object_storage.ObjectStorageClient(config, signer=signer)
+        namespace = object_storage.get_namespace().data
+        # update with your bucket name
+        bucket_name = "demo"
+        file_object_name = ctx.RequestURL()
+        if file_object_name.endswith("/"):
+            logging.getLogger().info("Adding index.html to reques URL " + file_object_name)
+            file_object_name += "index.html"
 
-    print("Vale of name = ", name, flush=True)
-    print("Exiting Python Hello World handler", flush=True)
-    return response.Response(
-        ctx, response_data=json.dumps(
-            {"message": "Hello {0}".format(name)}),
-        headers={"Content-Type": "application/json"}
-    )
+        # strip off the first character of the URI (i.e. the /)
+        file_object_name = file_object_name[1:]
+
+        obj = object_storage.get_object(namespace, bucket_name, file_object_name)
+        return response.Response(
+            ctx, response_data=obj.data.content,
+            headers={"Content-Type": obj.headers['Content-type']}
+        )
+    except (Exception) as e:
+        return response.Response(
+            ctx, response_data="500 Server error",
+            headers={"Content-Type": "text/plain"}
+        )

4da8c69ed2947e4d1688a83f9e979214531f6a85
ykourdi <ykourdi@riverbed.com>
2022-12-26 00:49:37 +0200

    up

--- func.py
+++ func.py
@@ -4,14 +4,22 @@ import logging
 import csv
 import pandas as pd
 
-
-
 import oci
 from fdk import response
 
 # create, read, update, and delete (CRUD)
 allowed_endpoint = ['', 'read', 'create', 'update', 'delete', 'listall']
 csv_api_url = "https://m7l2i2ximv4pqfi3xlea5yb53u.apigateway.il-jerusalem-1.oci.customer-oci.com/app2/getcsv"
+
+config = {
+    "tenancy": "ocid1.tenancy.oc1..aaaaaaaat3g6mubuxwcl26ef5tve3gpoz3bnrueskq7ma2fyjlk3jiiinxea",
+    "region": "il-jerusalem-1"
+}
+signer = oci.auth.signers.get_resource_principals_signer()
+object_storage = oci.object_storage.ObjectStorageClient(config, signer=signer)
+namespace = object_storage.get_namespace().data
+bucket_name = "assignment2"
+
 
 def handler(ctx, data: io.BytesIO = None):
     if None == ctx.RequestURL():
@@ -22,24 +30,16 @@ def handler(ctx, data: io.BytesIO = None):
         "tenancy": "ocid1.tenancy.oc1..aaaaaaaat3g6mubuxwcl26ef5tve3gpoz3bnrueskq7ma2fyjlk3jiiinxea",
         "region": "il-jerusalem-1"
     }
-    object_storage = oci.object_storage.ObjectStorageClient(config, signer=signer)
-    namespace = object_storage.get_namespace().data
-    bucket_name = "assignment2"
-    file_object_name = ctx.RequestURL()
     if ctx.Method() == "GET":
         try:
-            object_storage = oci.object_storage.ObjectStorageClient(config, signer=signer)
-            namespace = object_storage.get_namespace().data
-            # update with your bucket name
-            bucket_name = "assignment2"
-            file_object_name = ctx.RequestURL()
-            if file_object_name.endswith("/"):
+            endpoint = ctx.RequestURL()
+            if endpoint.endswith("/"):
                 obj = object_storage.get_object(namespace, bucket_name, 'index.html')
                 return response.Response(
                     ctx, response_data=obj.data.content,
                     headers={"Content-Type": obj.headers['Content-type']}
                 )
-            elif file_object_name.endswith("/getcsv"):
+            elif endpoint.endswith("/getcsv"):
                 obj = object_storage.get_object(namespace, bucket_name, 'db.csv')
                 return response.Response(
                     ctx, response_data=obj.data.content,
@@ -51,36 +51,8 @@ def handler(ctx, data: io.BytesIO = None):
             error_500(ctx)
     elif ctx.Method() == "POST":
         # obj = object_storage.get_object(namespace, bucket_name, 'csv.html')
-        try:
-            body = json.loads(data.getvalue())
-            name = body.get("name")
-            department = body.get("department")
-            birthday_month = body.get("birthday month")
-        except (Exception, ValueError) as ex:
-            print(str(ex))
+        new_record(ctx, data)
 
-        obj = object_storage.get_object(namespace, bucket_name, 'db.csv')
-        df = pd.read_csv(csv_api_url)
-        new_row = {'name':'Hyperion', 'department':24000, 'birthday month':'55days'}
-        df2 = df.append(new_row, ignore_index=True)
-        df2.to_csv()
-        object_storage.put_object(namespace, bucket_name, 'db.csv' ,df2.to_csv())
-        return response.Response(
-                ctx, response_data=json.dumps(
-                    {"Message": "Hello {0}, you work at {1} and your birthday is {2}".format(name,department, birthday_month),
-                     "csv": df.to_string(),
-                     "ctx.Config" : dict(ctx.Config()),
-                     "ctx.Headers" : ctx.Headers(),
-                     "ctx.AppID" : ctx.AppID(),
-                     "ctx.FnID" : ctx.FnID(),
-                     "ctx.CallID" : ctx.CallID(),
-                     "ctx.Format" : ctx.Format(),
-                     "ctx.Deadline" : ctx.Deadline(),
-                     "ctx.RequestURL": ctx.RequestURL(),
-                     "ctx.Method": ctx.Method()},
-                    sort_keys=True, indent=4),
-                headers={"Content-Type": "application/json"}
-        )
     else:
         return response.Response(
             ctx, response_data="405  Method not allowed",
@@ -101,12 +73,84 @@ def error_500(ctx):
 # PUT– This updates an object.
 # DELETE– This removes a record from the database.
 
-def new_record():
-    return
+def new_record(ctx, data):
+    try:
+        body = json.loads(data.getvalue())
+        name = body.get("name")
+        department = body.get("department")
+        birthday = body.get("birthday")
+        df = pd.read_csv(csv_api_url)
+        new_row = {'name': name, 'department': department, 'birthday month': birthday}
+        df2 = df.append(new_row, ignore_index=True)
+        df2.to_csv()
+        object_storage.put_object(namespace, bucket_name, 'db.csv', df2.to_csv())
+        return response.Response(
+            ctx, response_data=json.dumps(
+                {"Message": "Hello {0}, you work at {1} and your birthday is {2} - Bucket updated".format(name,
+                                                                                                          department,
+                                                                                                          birthday),
+                 "csv": df.to_string(),
+                 "ctx.Config": dict(ctx.Config()),
+                 "ctx.Headers": ctx.Headers(),
+                 "ctx.AppID": ctx.AppID(),
+                 "ctx.FnID": ctx.FnID(),
+                 "ctx.CallID": ctx.CallID(),
+                 "ctx.Format": ctx.Format(),
+                 "ctx.Deadline": ctx.Deadline(),
+                 "ctx.RequestURL": ctx.RequestURL(),
+                 "ctx.Method": ctx.Method()},
+                sort_keys=True, indent=4),
+            headers={"Content-Type": "application/json"}
+        )
+    except (Exception, ValueError) as ex:
+        error_500(ctx)
+
+
 def read_record():
     return
-def update_record():
-    return
+
+
+def update_record(ctx, data):
+    try:
+        body = json.loads(data.getvalue())
+        name = body.get("name")
+        department = body.get("department")
+        birthday = body.get("birthday month")
+        df = pd.read_csv(csv_api_url)
+
+        if not check_if_name_exist(df, name):
+            return response.Response(
+                ctx, response_data="404 Person not found! try a diff name",
+                headers={"Content-Type": "text/plain"}
+            )
+        else:
+            df.loc[df.name == "John Smith", 'department'] = department
+            df.loc[df.birthday == "John Smith", 'birthday'] = birthday
+            df.loc[df.name == name, 'name'] = name
+            # Update object store.
+            object_storage.put_object(namespace, bucket_name, 'db.csv', df.to_csv())
+            return response.Response(
+                ctx, response_data=json.dumps(
+                    {
+                        "Message": "Updated: {0}, work is now {1} and his birthday is {2} - Updated contact details.".format(
+                            name, department, birthday),
+                        "csv": df.to_string(),
+                        "ctx.Config": dict(ctx.Config()),
+                        "ctx.Headers": ctx.Headers(),
+                        "ctx.AppID": ctx.AppID(),
+                        "ctx.FnID": ctx.FnID(),
+                        "ctx.CallID": ctx.CallID(),
+                        "ctx.Format": ctx.Format(),
+                        "ctx.Deadline": ctx.Deadline(),
+                        "ctx.RequestURL": ctx.RequestURL(),
+                        "ctx.Method": ctx.Method()},
+                    sort_keys=True, indent=4),
+                headers={"Content-Type": "application/json"}
+            )
+    except (Exception, ValueError) as ex:
+        error_500(ctx)
+
+
 def delete_record():
     return
 
@@ -125,7 +169,6 @@ def route(ctx):
         return error_500(ctx)
 
 
-
 def route(endpoint):
     return
 
@@ -142,3 +185,9 @@ def redirect(ctx):
         return
     else:
         return error_500(ctx)
+
+
+def check_if_name_exist(df, name):
+    if df['name'].isin([name]).any():
+        return False
+    return True
